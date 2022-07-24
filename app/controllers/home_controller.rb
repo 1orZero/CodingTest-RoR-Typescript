99182ddf20216401d9de8d00dc3eadfe84e7944b
1orZero <Laokameng.6@gmail.com>
2022-07-24 03:35:08 +0000

    DONE

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@
 
 class HomeController < ApplicationController
   before_action :set_todo_item, only: [:edit_todo_item]
+  # around_action :get_todo_items, only: [:edit_todo_item, :reset_todo_items]
 
   def landing
     @todos = Todo.all.order(:id)
@@ -9,13 +10,22 @@ class HomeController < ApplicationController
 
   def edit_todo_item
     @todo_item.update(todo_item_params)
+    @todos = Todo.all.order(:id)
+    render :json => @todos
   end
 
   def reset_todo_items
     Todo.update_all(checked: false)
+    @todos = Todo.all.order(:id)
+    render :json => @todos
   end
 
   private
+
+  def get_todo_items
+    @todos = Todo.all.order(:id)
+    # render :json => @todos
+  end
 
   def todo_item_params
     params.permit(:id, :title, :checked)
--- app/javascript/components/TodoList/index.tsx
+++ app/javascript/components/TodoList/index.tsx
@@ -1,58 +1,62 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Container, ListGroup, Form } from "react-bootstrap";
 import { ResetButton } from "./uiComponent";
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
 type TodoItem = {
-  id: number;
-  title: string;
-  checked: boolean;
+	id: number;
+	title: string;
+	checked: boolean;
 };
 
 type Props = {
-  todoItems: TodoItem[];
+	todoItems: TodoItem[];
 };
 
 const TodoList: React.FC<Props> = ({ todoItems }) => {
-  useEffect(() => {
-    const token = document.querySelector(
-      "[name=csrf-token]"
-    ) as HTMLMetaElement;
-    axios.defaults.headers.common["X-CSRF-TOKEN"] = token.content;
-  }, []);
+	useEffect(() => {
+		const token = document.querySelector(
+			"[name=csrf-token]"
+		) as HTMLMetaElement;
+		axios.defaults.headers.common["X-CSRF-TOKEN"] = token.content;
+	}, []);
 
-  const checkBoxOnCheck = (
-    e: React.ChangeEvent<HTMLInputElement>,
-    todoItemId: number
-  ): void => {
-    axios.post("/todo", {
-      id: todoItemId,
-      checked: e.target.checked,
-    });
-  };
+	const [todos, setTodos] = useState(todoItems);
 
-  const resetButtonOnClick = (): void => {
-    axios.post("/reset").then(() => location.reload());
-  };
+	const checkBoxOnCheck = async (
+		e: React.ChangeEvent<HTMLInputElement>,
+		todoItemId: number
+	) => {
+		const res = (await axios.post("/todo", {
+			id: todoItemId,
+			checked: e.target.checked,
+		})) as AxiosResponse<TodoItem[]>;
+		setTodos(res.data);
+	};
 
-  return (
-    <Container>
-      <h3>2022 Wish List</h3>
-      <ListGroup>
-        {todoItems.map((todo) => (
-          <ListGroup.Item key={todo.id}>
-            <Form.Check
-              type="checkbox"
-              label={todo.title}
-              checked={todo.checked}
-              onChange={(e) => checkBoxOnCheck(e, todo.id)}
-            />
-          </ListGroup.Item>
-        ))}
-        <ResetButton onClick={resetButtonOnClick}>Reset</ResetButton>
-      </ListGroup>
-    </Container>
-  );
+	const resetButtonOnClick = async () => {
+		const res = (await axios.post("/reset")) as AxiosResponse<TodoItem[]>;
+		setTodos(res.data);
+	};
+
+	return (
+		<Container>
+			<h3>2022 Wish List</h3>
+			<ListGroup>
+				{todos.map((todo) => (
+					<ListGroup.Item key={todo.id}>
+						<Form.Check
+							type="checkbox"
+							label={todo.title}
+							checked={todo.checked}
+							onChange={(e) => checkBoxOnCheck(e, todo.id)}
+						/>
+					</ListGroup.Item>
+				))}
+				<ResetButton onClick={resetButtonOnClick}>Reset</ResetButton>
+			</ListGroup>
+		</Container>
+	);
 };
 
 export default TodoList;
