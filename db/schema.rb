bfdb13b5808ddf7326361ab0f7f961052c8dc9c8
1orZero <Laokameng.6@gmail.com>
2022-07-27 15:43:34 +0000

    add foreign key to todo table

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_05_213159) do
+ActiveRecord::Schema.define(version: 2022_07_27_143317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.boolean "checked", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_todos_on_category_id"
   end
 
+  add_foreign_key "todos", "categories"
 end
